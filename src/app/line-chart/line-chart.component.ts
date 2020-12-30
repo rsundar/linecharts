4e3b4b8c541b3f233d532803cef937bf0c6092a7
rsundar <rohan.s.1987@gmail.com>
2020-12-30 16:30:29 +0000

    Reading data from statistics.json

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartDataSets } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import statistics from '../../assets/statistics.json';
 
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.css']
 })
+
 export class LineChartComponent implements OnInit {
-	
-  public lineChartData: ChartDataSets[] = [
-	{ data: [85, 72, 78, 74, 77, 75], label: 'cpu-percentage' }
-  ];
   
-  public lineChartLabels: Label[] = ['1','2','3','4','5','6'];
+  stats: any = (statistics as any)._default;
+  
+  private percent: number[] = []; //this.stats.map(k => Object.keys(k).map(key => k[key]['percent']));
+  private time: string[] = []; //this.stats.map(k => Object.keys(k).map(key => k[key]['time']));
+  
+  //public lineChartData: ChartDataSets[] = [
+  //	{ data: [88, 75, 77, 75, 74, 78] , label: 'cpu-percentage' }
+  //];
+
+  public lineChartData: ChartDataSets[] = [];
+
+  //public lineChartLabels: Label[] = ['1', '2', '3', '4', '5', '6'];
+
+  public lineChartLabels: Label[] = [];
 
   public lineChartOptions = {
 	responsive: true,
@@ -30,7 +41,17 @@ export class LineChartComponent implements OnInit {
   public lineChartPlugins = [];
   public lineChartType = "line";
 
-  constructor() { }
+  constructor() {
+	for(var key of Object.keys(this.stats)) {
+		this.percent.push(this.stats[key]['percent']);
+		this.time.push(this.stats[key]['time']);
+	}
+	console.log("Percent: ",this.percent," Time= ",this.time);
+	this.lineChartData = [
+		{ data: this.percent, label: 'cpu-usage' }
+	];
+	this.lineChartLabels = this.time;
+  }
 
   ngOnInit(): void {
   }
